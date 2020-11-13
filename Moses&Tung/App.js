9f0cf369646685ebf907064dce326f969d963510
tUNg131 <halethuy.hlt@gmail.com>
2020-11-13 22:03:22 +0000

    Add Login App

--- Moses&Tung/App.js
+++ Moses&Tung/App.js
@@ -1,20 +1,18 @@
-import React from "react"
+import React, {Component} from "react"
+
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            firstName: "",
-            lastName: "",
-            isFriendly: false,
-            gender: "",
-            favColor: "blue"
+            username: "",
+            password: ""
         }
         this.handleChange = this.handleChange.bind(this)
     }
     
     handleChange(event) {
-        const {name, value, type, checked} = event.target
-        type === "checkbox" ? this.setState({ [name]: checked }) : this.setState({ [name]: value })
+        const {name, value, type} = event.target
+        this.setState({ [name]: value })
     }
     
     render() {
@@ -22,86 +20,21 @@ class App extends Component {
             <form>
                 <input 
                     type="text" 
-                    value={this.state.firstName} 
-                    name="firstName" 
-                    placeholder="First Name" 
+                    value={this.state.username} 
+                    name="username" 
+                    placeholder="User name" 
                     onChange={this.handleChange} 
                 />
                 <br />
                 <input 
                     type="text" 
-                    value={this.state.lastName} 
-                    name="lastName" 
-                    placeholder="Last Name" 
+                    value={this.state.password} 
+                    name="password" 
+                    placeholder="Password" 
                     onChange={this.handleChange} 
                 />
-                
-                {
-                    /**
-                     * Other useful form elements:
-                     * 
-                     *  <textarea /> element
-                     *  <input type="checkbox" />
-                     *  <input type="radio" />
-                     *  <select> and <option> elements
-                     */
-                }
-                
-                <textarea 
-                    value={"Some default value"}
-                    onChange={this.handleChange}
-                />
-                
                 <br />
-                
-                <label>
-                    <input 
-                        type="checkbox" 
-                        name="isFriendly"
-                        checked={this.state.isFriendly}
-                        onChange={this.handleChange}
-                    /> Is friendly?
-                </label>
-                <br />
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        value="male"
-                        checked={this.state.gender === "male"}
-                        onChange={this.handleChange}
-                    /> Male
-                </label>
-                <br />
-                <label>
-                    <input 
-                        type="radio" 
-                        name="gender"
-                        value="female"
-                        checked={this.state.gender === "female"}
-                        onChange={this.handleChange}
-                    /> Female
-                </label>
-                {/* Formik */}
-                <br />
-                
-                <label>Favorite Color:</label>
-                <select 
-                    value={this.state.favColor}
-                    onChange={this.handleChange}
-                    name="favColor"
-                >
-                    <option value="blue">Blue</option>
-                    <option value="green">Green</option>
-                    <option value="red">Red</option>
-                    <option value="orange">Orange</option>
-                    <option value="yellow">Yellow</option>
-                </select>
-                
-                <h1>{this.state.firstName} {this.state.lastName}</h1>
-                <h2>You are a {this.state.gender}</h2>
-                <h2>Your favorite color is {this.state.favColor}</h2>
-                <button>Submit</button>
+                <button>Login</button>
             </form>
         )
     }
